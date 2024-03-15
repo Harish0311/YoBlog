44b795d20b71c4d6f2de10d662e4f2c86a11394c
Harish0311 <harishgamer00@gmail.com>
2024-03-15 19:41:23 +0530

    Stylised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import Post from "./components/Post";
 import getRandomColor from "./components/getRandomColor";
 
+import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
+import { Button, Container } from "@mui/material";
+
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -16,16 +20,17 @@ function App() {
   }
 
   const handleButtonClick = () => {
-    if(inputValue!= ""&& textValue != ""){
-    const newPost = { input: inputValue, text: textValue ,color: getRandomColor()};
-    setPostedContent(prevContent => [...prevContent, newPost])}
-    else if (inputValue==""){
+    if (inputValue != "" && textValue != "") {
+      const newPost = { input: inputValue, text: textValue, color: getRandomColor() };
+      setPostedContent(prevContent => [...prevContent, newPost])
+    }
+    else if (inputValue == "") {
       alert("Please type your name before posting!!!")
     }
-    else if (textValue==""){
+    else if (textValue == "") {
       alert("Please type something before posting!!!")
     }
-  
+
 
   }
 
@@ -38,28 +43,43 @@ function App() {
   // )
 
   return (
-    <>
+    <Container>
 
       <h1 className="myTitle">Hello</h1>
 
-    
+
 
       <form>
-        <input placeholder="Name" value={inputValue} onChange={handleInputChange} />
-        <textarea placeholder="Say something..." value={textValue} onChange={handleTextChange} />
+        <TextField placeholder="Eg.Harish" color="primary"
+          InputProps={{
+            style: { backgroundColor: 'white' }
+          }} label="Name" value={inputValue} onChange={handleInputChange} />
+        <br /><br />
+        <div>
+          <TextField multiline rows={4}
+            InputProps={{
+              style: { backgroundColor: 'white' }
+            }} label="Name" value={inputValue} onChange={handleInputChange} placeholder="Say something..." label="Content" value={textValue} onChange={handleTextChange} /></div>
       </form>
+      <br />
+      <br />
       <div>
-        <button onClick={handleButtonClick}>Post</button>
+
+        <Button variant="contained" onClick={handleButtonClick} endIcon={<SendIcon />}>
+          Post
+        </Button>
       </div>
+      <br />
+      <br />
       {/* <Counter initialValue={12} />
       <Counter initialValue={15} reverse />
       <Counter initialValue={0} /> */}
 
       {postedContent.map((post, index) => (
         // <Posting key={index} input={post.input} text={post.text} />
-        <Post key={index} input={post.input} text={post.text} color = {post.color}/>
+        <Post key={index} input={post.input} text={post.text} color={post.color} />
       ))}
-    </>
+    </Container>
   )
 }
 
