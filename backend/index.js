8defe3f6d119b1d7fe72ec0565bdc5360117b967
Harish0311 <harishgamer00@gmail.com>
2024-03-16 15:18:46 +0530

    backend complete

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser= require("body-parser");
+
+const app= express()
+app.use(bodyParser.json())
+var post =[{input: "Harish", text: "Hello there! ", color: "#ff0000",postId: "abcd"}]
+
+app.get('/post',(req,res)=>{
+    res.json(post)
+})
+
+app.post('/post',(req,res)=>{
+    
+    const RecievedPost =req.body
+    post.unshift(RecievedPost)
+    res.json("Successfully Received thanks!")
+})
+
+app.patch('/post',(req,res)=>{
+    const RecievedChangesPost =req.body
+    const NeedChange = post.find(content => content.postId=== RecievedChangesPost.postId)
+    NeedChange.text=RecievedChangesPost.text
+    res.json("got it thanks!!")
+})
+
+app.delete('/post',(req,res)=>{
+    const PostIdtoDelete =req.query.postId
+    post = post.filter(Id=> Id.postId !== PostIdtoDelete)
+    res.json("RIP")
+})
+
+
+app.listen(2000)
+
+
