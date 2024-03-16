3f780c2a4970bbc9c6627bc0a37f6e869c599d08
Harish0311 <harishgamer00@gmail.com>
2024-03-16 18:58:16 +0530

    Basic database integration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const bodyParser= require("body-parser");
 const cors = require('cors');
-
+var mysql = require('mysql2');
 const app= express()
 app.use(bodyParser.json())
 app.use(cors());
-var post =[{input: "Harish", text: "Hello there! ", color: "#ff0000",postId: "abcd"}]
+// var post =[{input: "Harish", text: "Hello there! ", color: "#ff0000",postId: "abcd"}]
+var post;
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'YoBlog',
+    password: 'H@rish123'
+});
+
+// Test the connection
+pool.query('SELECT * FROM Card_Data', (err, results, fields) => {
+    if (err) {
+        console.error('Error executing query:', err);
+        return;
+    }
+    post = results;
+    // console.log('Query results:', results);
+});
 
 app.get('/post',(req,res)=>{
     res.json(post)
