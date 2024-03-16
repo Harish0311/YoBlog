40487d76aecb247fd5e0c0d81871175793e181bc
Harish0311 <harishgamer00@gmail.com>
2024-03-16 20:04:56 +0530

    complete backend integration

--- backend/index.js
+++ backend/index.js
@@ -15,36 +15,60 @@ const pool = mysql.createPool({
 });
 
 // Test the connection
-pool.query('SELECT * FROM Card_Data', (err, results, fields) => {
-    if (err) {
-        console.error('Error executing query:', err);
-        return;
-    }
-    post = results;
-    // console.log('Query results:', results);
-});
+// pool.query('SELECT * FROM Card_Data', (err, results, fields) => {
+//     if (err) {
+//         console.error('Error executing query:', err);
+//         return;
+//     }
+//     post = results;
+//     // console.log('Query results:', results);
+// });
 
 app.get('/post',(req,res)=>{
+    pool.query('SELECT * FROM Card_Data', (err, results, fields) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        post = results;
+        // console.log('Query results:', results);
+    });
     res.json(post)
-})
+});
 
 app.post('/post',(req,res)=>{
     
     const RecievedPost =req.body
-    post.unshift(RecievedPost)
+    pool.query('INSERT INTO Card_Data SET ?', RecievedPost, (err, results) => {
+        if (err) {
+            console.error('Error inserting row:', err);
+            return;
+        }
+        console.log('New row inserted with ID:', results.insertId);
+    });
+    // post.unshift(RecievedPost)
     res.json("Successfully Received thanks!")
 })
 
 app.patch('/post',(req,res)=>{
     const RecievedChangesPost =req.body
     const NeedChange = post.find(content => content.postId=== RecievedChangesPost.postId)
+    pool.query('UPDATE Card_Data SET text = ? WHERE postId = ?',[RecievedChangesPost.text,RecievedChangesPost.postId])
     NeedChange.text=RecievedChangesPost.text
     res.json("got it thanks!!")
 })
 
 app.delete('/post',(req,res)=>{
     const PostIdtoDelete =req.query.postId
-    post = post.filter(Id=> Id.postId !== PostIdtoDelete)
+
+    pool.query('DELETE FROM Card_Data WHERE postId = ?',PostIdtoDelete,(err,results)=>{
+        if (err) {
+            console.error('Error deleting row:', err);
+            return;
+        }
+        console.log('New row inserted with ID:', results.insertId);
+    })
+    // post = post.filter(Id=> Id.postId !== PostIdtoDelete)
     res.json("RIP")
 })
 
